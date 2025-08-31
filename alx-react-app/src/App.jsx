35fddab361bdf5095a57812552c2c2a8ff4a590e
Brian Richu <richubrian427@gmail.com>
2025-08-31 21:04:47 +0300

    changed location og the components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,11 @@
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Footer from './Footer'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
