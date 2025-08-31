c60993e84c2d46709d50a8d8192044969fe1dc76
Brian Richu <richubrian427@gmail.com>
2025-08-31 21:11:29 +0300

    create a user profile using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 import Footer from './components/Footer'
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
        <WelcomeMessage />
        <Header />
        <MainContent />
+       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
        <Footer />
     </>
    
