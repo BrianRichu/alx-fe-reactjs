7b25d60bb6d459ddf55af464dfd4f80035d6e91e
Brian Richu <richubrian427@gmail.com>
2025-09-21 19:39:38 +0300

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.github.com/users";
-const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const fetchUser = async (username) => {
+export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${BASE_URL}/${username}`, {
       headers: {
-        Authorization: API_KEY ? `token ${API_KEY}` : "",
+        Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+          ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+          : "",
       },
     });
     return response.data;
   } catch (error) {
-    throw error.response?.data || { message: "User not found" };
+    throw error;
   }
 };
